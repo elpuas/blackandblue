6526029585a860329307de77917f12e4a50bd1bb
elpuas <elpuas@gmail.com>
2018-05-29 20:19:18 +0200

    functions

--- functions.php
+++ functions.php
@@ -2,6 +2,14 @@
 function blackandbluefishing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'blackandbluefishing-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Mono:400|Open+Sans+Condensed:700|Roboto+Condensed', false );
-    wp_enqueue_script( 'blackandbluefishing-scripts', get_stylesheet_directory() . '/bb-scripts.js', array(), true );
+
 }
 add_action( 'wp_enqueue_scripts', 'blackandbluefishing_enqueue_styles' );
+
+
+add_action( 'wp_enqueue_scripts', 'blackandbluefishing_scripts' );
+
+function blackandbluefishing_scripts() {
+wp_enqueue_script( 'blackandbluefishing-script', get_stylesheet_directory_uri() . '/bb_script.js', array( 'jquery' )
+);
+}
