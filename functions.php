e54cdc5ee429304d46002e47981c6c3172e338fb
elpuas <elpuas@gmail.com>
2018-07-03 14:19:58 -0400

    new fonts

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function blackandbluefishing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'blackandbluefishing-google-fonts', 'https://fonts.googleapis.com/css?family=Fira+Mono:400|Open+Sans+Condensed:700|Roboto+Condensed', false );
+    wp_enqueue_style( 'blackandbluefishing-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:700|Passion+One', false );
 
 }
 add_action( 'wp_enqueue_scripts', 'blackandbluefishing_enqueue_styles' );
