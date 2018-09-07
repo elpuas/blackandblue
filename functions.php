0ee850d48a788fac7cf7b73ab679d2fbabb6b3d0
elpuas <elpuas@gmail.com>
2018-09-07 14:25:01 -0400

    hidde elements for simple users it was a table not a div

--- functions.php
+++ functions.php
@@ -60,7 +60,7 @@ function blackandblue_adminizer(){
       .et_pb_toggle_builder_wrapper, .wp-editor-container, 
       .recurrence-row tribe-datetime-block, #event_tribe_venue, 
       #event_tribe_organizer, #event_url, #event_cost, 
-      .eventBritePluginPlug, #et_settings_meta_box, #tagsdiv-post_tag
+      .eventBritePluginPlug, #et_settings_meta_box, #tagsdiv-post_tag,
       #tribe_events_event_options, #menu-posts-tribe_events ul.wp-submenu-wrap li:nth-child(n+4)  {
         display: none !important;
       }
