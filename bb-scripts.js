c5cf9072b6b16fa3ec7b7864b7d3340d583f28e9
elpuas <elpuas@gmail.com>
2018-09-10 12:37:29 -0400

    updated name

--- bb-scripts.js
+++ bb-scripts.js
@@ -58,12 +58,18 @@ jQuery(document).ready(function($){
             $('a[href*="panama"]').each(function() {
                 console.log('Contains question mark');
             });
+
+            $('a[href*="bermuda"]').each(function() {
+                console.log('Contains question mark');
+            });
             
             $(this).find('a').attr('href', 'http://blackandbluefishing.elpuasdev.com/wp-admin/post-new.php?post_type=tribe_events');
 
             $(this).find('a:contains("Panama")').addClass('panama');
 
-            $(this).find('a:not(.panama)').css('display', 'none');
+            $(this).find('a:contains("Bermuda")').addClass('bermuda');
+
+            $(this).find('a:not(.panama), a:not(.bermuda) ').css('display', 'none');
 
             });
 
