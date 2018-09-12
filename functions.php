46d6d20847bd7ea7cdff16bef8e7ab7358657843
elpuas <elpuas@gmail.com>
2018-09-12 13:08:16 -0400

    bug fix : 1

--- functions.php
+++ functions.php
@@ -155,5 +155,23 @@ function wpse23007_redirect(){
     
     exit;
   }
+
 }
 add_action('init','wpse23007_redirect');
+
+
+/*
+ Remove Admin Bar
+*/
+
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+
+if (!current_user_can('administrator') && !is_admin()) {
+
+  show_admin_bar(false);
+
+  }
+
+}
